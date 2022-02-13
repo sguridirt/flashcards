cf411850c0eb14d72f507c4b8387dbcb94792c8b
sguridirt <34144826+sguridirt@users.noreply.github.com>
2022-02-13 17:25:41 -0300

    add start button (without functionality)

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,6 +2,8 @@
   import AnswerForm from "../components/AnswerForm.svelte";
   import Card from "../components/Card.svelte";
 
+  let started = false;
+
   let deckName: string = "Duolingo word review";
 
   let inputStr = "";
@@ -14,6 +16,14 @@
 <main class="flex flex-col items-center max-w-2xl p-8 gap-5 my-0 mx-auto">
   <h2 class="self-start uppercase text-sm text-gray-700">{deckName}</h2>
   <Card />
-  <AnswerForm />
-  <p class="text-gray-700 italic">(Press enter to continue)</p>
+  {#if started}
+    <AnswerForm />
+    <p class="text-gray-700 italic">
+      (Press <kbd class="tracking-normal">enter</kbd> to continue)
+    </p>
+  {:else}
+    <button class="p-3 w-full font-bold text-white bg-black rounded-sm uppercase"
+      >START</button
+    >
+  {/if}
 </main>
