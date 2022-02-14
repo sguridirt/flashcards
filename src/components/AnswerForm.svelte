51da1253325b72e33bbf47a8521420b416d2c4f5
sguridirt <34144826+sguridirt@users.noreply.github.com>
2022-02-14 10:45:19 -0300

    handle form submits and form display

--- src/components/AnswerForm.svelte
+++ src/components/AnswerForm.svelte
@@ -1,15 +1,38 @@
 <script lang="ts">
-  function handleSubmit(event) {}
+  import { fly } from "svelte/transition";
+  import { createEventDispatcher } from "svelte";
+
+  import { shouldCleanForm } from "../stores/formStore";
+
+  const dispatch = createEventDispatcher();
+
+  export let prompt: string;
+
+  let inputValue: string = "";
+
+  function handleSubmit() {
+    if (inputValue.length !== 0) {
+      dispatch("submit", {
+        text: inputValue,
+      });
+    }
+  }
+
+  $: {
+    if ($shouldCleanForm) inputValue = "";
+  }
 </script>
 
 <form
-  class="w-full mb-2 p-4 border border-black rounded-sm"
+  class="mb-2 w-full rounded-sm border border-black p-4"
   on:submit|preventDefault={handleSubmit}
+  in:fly={{ x: -200, duration: 600 }}
 >
-  <p class="mb-2 text-sm text-gray-700">Type your answer 👇</p>
+  <p class="mb-2 text-sm text-gray-700">{prompt}</p>
   <input
     type="text"
     placeholder="answer..."
-    class="w-full h-12 border-b-2 border-black focus:outline-none"
+    class="h-12 w-full border-b-2 border-black focus:outline-none"
+    bind:value={inputValue}
   />
 </form>
